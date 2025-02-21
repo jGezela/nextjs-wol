ed277d186bbedc00d2222852d5522c09d6cfd55f
Jakub Gezela <pl.jgezela@gmail.com>
2025-02-21 23:57:12 +0100

    Shadcn init + UI skeleton

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <section>
+      <h1>Settings</h1>
+    </section>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import {
+  CirclePower,
+  House, 
+  Settings,
+} from "lucide-react";
+
+interface NavbarItem {
+  title: string;
+  href: string;
+  icon: React.ReactElement;
+}
+
+const navbarItems: NavbarItem[] = [
+  { title: "Home", href: "/", icon: <House /> },
+  { title: "Settings", href: "/settings", icon: <Settings /> }, 
+]
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="py-4 w-20 h-full bg-[#222222] rounded-2xl flex flex-col items-center gap-3">
+      <div className="mb-4">
+        <CirclePower color="#FFFFFF" size={38} className="-rotate-12"/>
+        <p className="mt-1 text-white text-center font-bold">WoL</p>
+      </div>
+
+      { navbarItems.map(item =>
+        <TooltipProvider key={ item.title }>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link 
+              href={ item.href }
+              className={clsx(
+                "w-14 h-14 flex justify-center items-center rounded-full transition-colors text-white hover:bg-white hover:text-[#222222] focus:bg-white focus:text-[#222222] focus:outline-0",
+                {
+                  "text-[#222222] bg-white": pathname === item.href
+                }
+              )}
+            >
+              { item.icon }
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right" sideOffset={24} className="text-sm rounded-full bg-[#222222]">
+            { item.title }
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      )}
+    </nav>
+  )
+}
